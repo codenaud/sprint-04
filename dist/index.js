11ef8ef55b2c2f788fe2a3c932857a9c7a2384c5
codenaud <codenaud@gmail.com>
2023-12-12 18:06:21 +0100

    Finishing exercice 3

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 "use strict";
 console.log('hello');
-const btn = document.querySelector('button');
-console.log(btn);
+const btnNextJoke = document.querySelector('#next-joke');
 function getJokes() {
     const headers = new Headers();
     headers.set('Accept', 'application/json');
@@ -22,17 +21,55 @@ if (!result) {
 function initialJoke() {
     getJokes().then((jokes) => {
         if (result) {
-            result.innerHTML = `${jokes.joke}`;
+            console.log(jokes.joke);
+            result.innerHTML = jokes.joke;
         }
     });
 }
 document.addEventListener('DOMContentLoaded', initialJoke);
-if (btn instanceof Element) {
-    btn.addEventListener('click', () => {
+if (btnNextJoke instanceof Element) {
+    btnNextJoke.addEventListener('click', () => {
         getJokes().then((jokes) => {
             console.log(jokes.joke);
-            result.innerHTML = `${jokes.joke}`;
+            result.innerHTML = jokes.joke;
         });
     });
 }
+const scoreButtons = document.querySelectorAll('.score-button');
+let reportJokes = [];
+let currentScore = null;
+function displayJoke(joke) {
+    const result = document.querySelector('#jokes');
+    if (!result) {
+        throw new Error('No element with class `jokes`');
+    }
+    result.innerHTML = joke;
+}
+function handleScoreButtonClick(score) {
+    currentScore = score;
+    console.log(`Puntuación actual: ${currentScore}`);
+}
+document.addEventListener('DOMContentLoaded', initialJoke);
+if (btnNextJoke instanceof Element) {
+    btnNextJoke.addEventListener('click', () => {
+        var _a;
+        const currentJoke = (_a = document.querySelector('#jokes')) === null || _a === void 0 ? void 0 : _a.textContent;
+        if (currentJoke && currentScore !== null) {
+            const currentDate = new Date().toISOString();
+            const jokeEntry = { joke: currentJoke, score: currentScore, date: currentDate };
+            reportJokes.push(jokeEntry);
+            console.log('Reported Jokes:', reportJokes);
+            currentScore = null;
+            getJokes().then((jokes) => {
+                displayJoke(jokes.joke);
+            });
+        }
+    });
+}
+scoreButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const score = index + 1;
+        handleScoreButtonClick(score);
+    });
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@
 
 console.log('hello');
 
-const btn = document.querySelector('button');
-
-console.log(btn);
+const btnNextJoke = document.querySelector('#next-joke');
 
 // icanhazdadjoke.com API URL: https://icanhazdadjoke.com/
 
@@ -49,7 +47,8 @@ if (!result) {
 function initialJoke() {
   getJokes().then((jokes) => {
     if (result) {
-      result.innerHTML = `${jokes.joke}`;
+      console.log(jokes.joke);
+      result.innerHTML = jokes.joke;
     }
   });
 }
@@ -58,25 +57,62 @@ function initialJoke() {
 document.addEventListener('DOMContentLoaded', initialJoke);
 
 // Click event listener for the button
-if (btn instanceof Element) {
-  btn.addEventListener('click', () => {
+if (btnNextJoke instanceof Element) {
+  btnNextJoke.addEventListener('click', () => {
     getJokes().then((jokes) => {
       console.log(jokes.joke);
-      result.innerHTML = `${jokes.joke}`;
+      result.innerHTML = jokes.joke;
     });
   });
 }
 
 // Exercici 2 [reportAcudits]
+const scoreButtons = document.querySelectorAll('.score-button');
+let reportJokes: { joke: string; score: number; date: string }[] = [];
+let currentScore: number | null = null; // Variable para almacenar la puntuación actual
 
-interface ReportJokes {
-  joke: string;
-  score: number;
-  date: string;
+function displayJoke(joke: string) {
+  const result = document.querySelector('#jokes');
+  if (!result) {
+    throw new Error('No element with class `jokes`');
+  }
+
+  result.innerHTML = joke;
 }
 
-/* const reportJokes {
-  joke: "...",
-  score:1,
-  date: ...
-}; */
+function handleScoreButtonClick(score: number) {
+  // Actualiza la variable global de puntuación actual
+  currentScore = score;
+  console.log(`Puntuación actual: ${currentScore}`);
+}
+
+document.addEventListener('DOMContentLoaded', initialJoke);
+
+if (btnNextJoke instanceof Element) {
+  btnNextJoke.addEventListener('click', () => {
+    const currentJoke = document.querySelector('#jokes')?.textContent;
+    if (currentJoke && currentScore !== null) {
+      const currentDate = new Date().toISOString();
+      const jokeEntry = { joke: currentJoke, score: currentScore, date: currentDate };
+      reportJokes.push(jokeEntry);
+
+      // Log the current state of reportJokes
+      console.log('Reported Jokes:', reportJokes);
+
+      // Restablece la puntuación actual a null después de agregar la broma al array
+      currentScore = null;
+
+      // Muestra la siguiente broma
+      getJokes().then((jokes) => {
+        displayJoke(jokes.joke);
+      });
+    }
+  });
+}
+
+scoreButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const score = index + 1; // Since index is zero-based
+    handleScoreButtonClick(score);
+  });
+});
